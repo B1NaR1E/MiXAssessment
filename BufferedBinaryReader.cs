9e97514933156a5409fc6d38ca81b96af12c3d78
Sidney Roth <sidner01@out.co.za>
2023-08-03 23:02:26 +0200

    Test

---
+++ BufferedBinaryReader.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiXAssessment
+{
+    internal class BufferedBinaryReader : IDisposable
+    {
+        private readonly Stream _stream;
+        private readonly byte[] _buffer;
+        private readonly int _bufferSize;
+        private int _bufferOffset;
+        private int _numBufferedBytes;
+
+        public BufferedBinaryReader(Stream stream, int bufferSize)
+        {
+            _stream = stream;
+            _bufferSize = bufferSize;
+            _buffer = new byte[bufferSize];
+            _bufferOffset = bufferSize;
+        }
+
+        public async Task<bool> FillBufferAsync()
+        {
+            var numBytesUnread = _bufferSize - _bufferOffset;
+            var numBytesToRead = _bufferSize - numBytesUnread;
+
+            _bufferOffset = 0;
+
+            _numBufferedBytes = numBytesUnread;
+
+            if (numBytesUnread > 0)
+            {
+                Buffer.BlockCopy(_buffer, numBytesToRead, _buffer, 0, numBytesToRead);
+            }
+
+            while (numBytesToRead > 0)
+            {
+                var numBytesRead = await _stream.ReadAsync(_buffer, numBytesUnread, numBytesToRead);
+
+                if (numBytesRead == 0)
+                {
+                    return false;
+                }
+                _numBufferedBytes += numBytesRead;
+                numBytesToRead -= numBytesRead;
+                numBytesUnread += numBytesRead;
+            }
+            return true;
+        }
+
+        //public bool CanRead { get { return _stream.P} }
+
+        //public ushort ReadUInt16() 
+        //{
+        //    var tempBuffer = new byte[2];
+
+        //    Buffer.BlockCopy(_buffer, _bufferOffset, tempBuffer, 0, tempBuffer.Length);
+
+        //    var valll = BitConverter.ToUInt16(tempBuffer, 0);
+
+        //    var val = _buffer[_bufferOffset] | _buffer[_bufferOffset + 1] << 8;
+
+        //    _bufferOffset += tempBuffer.Length;
+        //    return val;
+        //}
+
+        public int ReadInt32()
+        {
+            var tempBuffer = new byte[4];
+
+            Buffer.BlockCopy(_buffer, _bufferOffset, tempBuffer, 0, tempBuffer.Length);
+
+            var val = BitConverter.ToInt32(tempBuffer, 0);
+
+            _bufferOffset += tempBuffer.Length;
+
+            return val;
+        }
+
+        public string ReadAscii()
+        {
+            //List<byte> tempBuffer = new List<byte>();
+
+            //int b;
+            //int index = _bufferOffset;
+            //while ((b = (int)_buffer[index]) != 0x00)
+            //{
+            //    tempBuffer.Add((byte)b);
+            //    ++index;
+            //}
+            //_bufferOffset += tempBuffer.Count + 1;
+            var tempBuffer = new byte[11];
+
+            Buffer.BlockCopy(_buffer, _bufferOffset, tempBuffer, 0, tempBuffer.Length);
+            _bufferOffset += tempBuffer.Length + 1;
+            return Encoding.ASCII.GetString(tempBuffer.ToArray());
+        }
+
+        public float ReadSingle()
+        {
+            var tempBuffer = new byte[4];
+
+            Buffer.BlockCopy(_buffer, _bufferOffset, tempBuffer, 0, tempBuffer.Length);
+
+            var val = BitConverter.ToSingle(tempBuffer, 0);
+
+            //var val = (float)(_buffer[_bufferOffset] | _buffer[_bufferOffset + 1] | _buffer[_bufferOffset + 2] | _buffer[_bufferOffset + 3] << 8);
+            _bufferOffset += tempBuffer.Length;
+            return val;
+        }
+
+        public ulong ReadUInt64()
+        {
+            var tempBuffer = new byte[8];
+
+            Buffer.BlockCopy(_buffer, _bufferOffset, tempBuffer, 0, tempBuffer.Length);
+
+            var val = BitConverter.ToUInt64(tempBuffer, 0);
+
+            _bufferOffset += tempBuffer.Length;
+
+            return val;
+        }
+
+        public void Dispose()
+        {
+            _stream.Close();
+            //throw new NotImplementedException();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,56 +2,63 @@
 using MiXAssessment.Extentions;
 using MiXAssessment.Models;
 using System.Diagnostics;
+using System.IO;
 
 var stopWatch = new Stopwatch();
 var locations = InstantiateLocations();
 var tree = new KdBush(2);
 long totalTime  = 0;
 
-try
+//try
 {
+    //Console.WriteLine("Loading vehicles into K Dimentional Tree.\n");
+
+    //stopWatch.Start();
+
+    //LoadDataIntoKdTree();
+
+    //stopWatch.Stop();
+    //totalTime += stopWatch.ElapsedMilliseconds;
+    //Console.WriteLine($"Data loaded. Time Elapsed: {stopWatch.ElapsedMilliseconds} ms.\n");
+
     Console.WriteLine("Loading vehicles into K Dimentional Tree.\n");
 
-    stopWatch.Start();
+    await LoadDataIntoKdTree();
 
-    LoadDataIntoKdTree();
+    //Console.WriteLine("Starting Nearest Neighbors algorithm.\n");
 
-    stopWatch.Stop();
-    totalTime += stopWatch.ElapsedMilliseconds;
-    Console.WriteLine($"Data loaded. Time Elapsed: {stopWatch.ElapsedMilliseconds} ms.\n");
+    //stopWatch.Restart();
 
-    Console.WriteLine("Starting Nearest Neighbors algorithm.\n");
+    //foreach (var location in locations)
+    //{
+    //    var vehicle = tree.NearestNeighbors(new double[] { location.Coordinates.Longitude, location.Coordinates.Latitude }, 1).FirstOrDefault();
+    //    Console.WriteLine($"The closest vehicle to position: {location.PositionId} is vehicleId: {vehicle!.Item2}. With Distance: {vehicle!.Item1} m");
+    //}
 
-    stopWatch.Restart();
+    //stopWatch.Stop();
 
-    foreach (var location in locations)
-    {
-        var vehicle = tree.NearestNeighbors(new double[] { location.Coordinates.Longitude, location.Coordinates.Latitude }, 1).FirstOrDefault();
-        Console.WriteLine($"The closest vehicle to position: {location.PositionId} is vehicleId: {vehicle!.Item2}. With Distance: {vehicle!.Item1} m");
-    }
+    //totalTime += stopWatch.ElapsedMilliseconds;
 
-    stopWatch.Stop();
+    //Console.WriteLine();
 
-    totalTime += stopWatch.ElapsedMilliseconds;
+    //Console.WriteLine($"Algorithm completed. Time Elapsed: {stopWatch.ElapsedMilliseconds} ms.\n");
 
-    Console.WriteLine();
-
-    Console.WriteLine($"Algorithm completed. Time Elapsed: {stopWatch.ElapsedMilliseconds} ms.\n");
-
-    Console.WriteLine($"Total Time(s): {totalTime / 1000} s.\nTotal Time(ms): {totalTime} ms.");
+    //Console.WriteLine($"Total Time(s): {totalTime / 1000} s.\nTotal Time(ms): {totalTime} ms.");
 
 }
-catch (Exception ex)
-{
-    Console.WriteLine($"An unexpected error has occurred. Message: {ex.Message}");
-}
+//catch (Exception ex)
+//{
+//    Console.WriteLine($"An unexpected error has occurred. Message: {ex.Message}");
+//}
 
 #region Helper Methods
 
 // This method takes on average 10 seconds to read all the data and build the K Dimentional Tree. Need to find a way to speed this method up.
-void LoadDataIntoKdTree()
+async Task LoadDataIntoKdTree()
 {
+    int count = 0;
     var result = new List<Vehicle>();
+    Stopwatch sWatch = new Stopwatch();
 
     if (!File.Exists("VehiclePositions.dat"))
     {
@@ -60,23 +67,90 @@ void LoadDataIntoKdTree()
 
     var dataBytes = File.ReadAllBytes("VehiclePositions.dat");
 
+    var dataL = dataBytes.Length / 2000001;
+    var bufferSize = 200 * dataL;
+
     using (var stream = new MemoryStream(dataBytes))
     {
+        sWatch.Start();
+
         using (System.IO.BinaryReader reader = new System.IO.BinaryReader(stream))
         {
-            while (reader.BaseStream.Position < reader.BaseStream.Length)
+            do
             {
                 var id = reader.ReadInt32();
                 var reg = reader.ReadAscii();
                 var lat = reader.ReadSingle();
                 var lon = reader.ReadSingle();
                 var time = reader.ReadUInt64();
-
-                tree.InsertRecord(new double[] { lon, lat }, id);
+                ++count;
             }
+            while (reader.BaseStream.Position < reader.BaseStream.Length);
+
         }
+
+        sWatch.Stop();
+        Console.WriteLine($"Data loaded. Time Elapsed: {sWatch.ElapsedMilliseconds} ms. Total Rec: {count}\n");
+        count = 0;
+
     }
+
+    using (var stream = new MemoryStream(dataBytes))
+    {
+        sWatch.Restart();
+        using (BufferedBinaryReader reader = new BufferedBinaryReader(stream, bufferSize))
+        {
+            do
+            {
+                while (await reader.FillBufferAsync())
+                {
+                    for (int i = 0; i < 199; i++)
+                    {
+                        var id = reader.ReadInt32();
+                        var reg = reader.ReadAscii();
+                        var lat = reader.ReadSingle();
+                        var lon = reader.ReadSingle();
+                        var time = reader.ReadUInt64();
+                        Console.WriteLine($"String byte length = {reg}");
+                    }
+                    ++count;
+                }
+            }
+            while (stream.Position < stream.Length);
+        }
+
+        sWatch.Stop();
+        Console.WriteLine($"Data loaded. Time Elapsed: {sWatch.ElapsedMilliseconds} ms. Total Rec: {count}\n"); 
+    }
+
+    //return count;
 }
+
+//int LoadData()
+//{
+//    int count = 0;
+//    if (!File.Exists("VehiclePositions.dat"))
+//    {
+//        Console.WriteLine($"The data file was not found.");
+//    }
+
+//    var dataBytes = File.ReadAllBytes("VehiclePositions.dat");
+
+//    var reader = new BufferedBinaryReader(dataBytes);
+
+//    do
+//    {
+//        var id = reader.ReadInt32();
+//        var reg = reader.ReadAscii();
+//        var lat = reader.ReadSingle();
+//        var lon = reader.ReadSingle();
+//        var time = reader.ReadUInt64();
+//        ++count;
+//    }
+//    while (reader.Position < dataBytes.Length);
+
+//    return count;
+//}
 
 List<Position> InstantiateLocations()
 {
